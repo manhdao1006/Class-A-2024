77f550ac552c10dd7423b6d2c9bd5f33262912c8
Manh Dao <manhdao1006@gmail.com>
2024-09-28 10:46:15 +0700

    first commit

---
+++ app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Task\CreateRequest;
+use App\Http\Requests\Api\Task\UpdateRequest;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use App\Services\TaskService;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class TaskController extends Controller
+{
+    protected $taskService;
+
+    public function __construct(TaskService $taskService){
+        $this->taskService = $taskService;
+    }
+
+    public function index()
+    {
+        return response()->json(['message' => 'Hello World!']);
+    }
+
+    public function store(CreateRequest $createRequest)
+    {  
+        $request = $createRequest->validated();
+        $result = $this->taskService->create($request);
+
+        if ($result) {
+            return new TaskResource($result);
+        }
+
+        return response()->json([
+            'message' => 'error'
+
+        ]);
+    }
+
+    public function show(Task $task)
+    {
+        return new TaskResource($task);
+    }
+
+    public function update(UpdateRequest $updateRequest, Task $task)
+    {
+        $request = $updateRequest->validated();
+        $result = $this->taskService->edit($task, $request);
+
+        if ($result) {
+            return new TaskResource($result);
+        }
+
+        return response()->json([
+            'message' => 'error'
+
+        ]);
+    }
+
+    // soft delete
+    public function destroy(Task $task)
+    {
+        try {
+            $task->delete();
+
+            return response()->json([
+                'message' => 'ok'
+    
+            ]);
+        } catch (Exception $exception) {
+            return response()->json([
+                'message' => $exception
+    
+            ]);
+        }
+    }
+}
